b1a9189b210828f29126c44f7a6a1541bda6041b
Nicole Dow <nikki.dow71@gmail.com>
2019-08-28 14:32:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
+require_relative '../models/team'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do 
+      erb :team_and_motto
+    end
+    
+    post '/teams' do
+      @team = Team.new(params["team"]["name"], params["team"]["motto"])
+      #binding.pry
+      @team_members_arr = params["team"]["members"]
+      
+      erb :team
+    end
 
 
 end
---
+++ app/models/hero.rb
@@ -0,0 +1,11 @@
+class Hero
+  attr_accessor :name, :power, :motto
+  
+  def initialize(name, power, motto)
+    @name = name
+    @power = power
+    @motto = motto
+  end
+  
+  
+end
